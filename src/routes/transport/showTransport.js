4fe9d7687552abbdab9159def8ef4e85994eefee
Prafful Kumar <kumarprafful.54@gmail.com>
2019-02-28 01:34:50 +0530

    bug

--- src/routes/transport/showTransport.js
+++ src/routes/transport/showTransport.js
@@ -20,22 +20,24 @@ class ShowTransport extends Component {
   renderTransports() {
     const userId = localStorage.userId;
     const apiURL = `https://sahayata-farmer.herokuapp.com/sahayata/transport/${userId}`;
-    //
-    // axios.get(apiURL)
-    // .then(res => {
-    //   console.log(res);
-    // })
-    // .catch(error => {
-    //   console.log(error);
-    // })
+    console.log("HET");
+    axios.get(apiURL)
+    .then(res => {
+      console.log(res);
+      this.setState({data:res});
+      console.log(this.state);
+    })
+    .catch(error => {
+      console.log(error);
+    })
 
-    var data = [
-      {"_id":"5c759ceb07ac51001769ff67","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
-      {"_id":"5c759d7107ac51001769ff69","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
-      {"_id":"5c759d7307ac51001769ff6a","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
-      {"_id":"5c759e7807ac51001769ff6b","type":"sd,","vehicleNumber":"lkjv","capacity":"slkdj","price":"lkj","__v":0},
-      {"_id":"5c759f2507ac51001769ff6c","type":"df","vehicleNumber":"fe","capacity":"wdfh","price":"dhdfh","__v":0}
-    ]
+    // var data = [
+    //   {"_id":"5c759ceb07ac51001769ff67","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
+    //   {"_id":"5c759d7107ac51001769ff69","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
+    //   {"_id":"5c759d7307ac51001769ff6a","type":"sdkj","vehicleNumber":"lkj","capacity":"jkl","price":"lkj","__v":0},
+    //   {"_id":"5c759e7807ac51001769ff6b","type":"sd,","vehicleNumber":"lkjv","capacity":"slkdj","price":"lkj","__v":0},
+    //   {"_id":"5c759f2507ac51001769ff6c","type":"df","vehicleNumber":"fe","capacity":"wdfh","price":"dhdfh","__v":0}
+    // ]
 
     // this.setState({data:data});
     // console.log(this.state.data);
@@ -52,7 +54,7 @@ class ShowTransport extends Component {
   render() {
     return (
     <CardBody>
-      {this.renderTransports()};
+      {this.renderTransports};
     </CardBody>
     );
   }
